63c37b4689b57a55808d4d5b746be7075d218539
“XiaoyangLYY” <xiaoyanglyu760514@gmail.com>
2026-04-28 21:20:23 -0400

    feat: expose public API in __init__.py

---
+++ src/minvar_l2/__init__.py
@@ -0,0 +1,5 @@
+from minvar_l2.optimizer import min_variance_l2
+from minvar_l2.covariance import sample_covariance, ewma_covariance
+from minvar_l2.metrics import performance_summary
+from minvar_l2.backtest import run_monthly_backtest
+
